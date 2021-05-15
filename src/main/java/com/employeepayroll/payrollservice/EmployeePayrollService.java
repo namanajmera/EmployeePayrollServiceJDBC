683124649b026181fddc294a18858d9307fe9e2a
Naman <cool.naman.ajmera@gmail.com>
2021-05-15 16:04:57 +0530

    Added: Connection is created.

---
+++ src/main/java/com/employeepayroll/payrollservice/EmployeePayrollService.java
@@ -0,0 +1,64 @@
+package com.employeepayroll.payrollservice;
+
+import com.employeepayroll.ioservice.FileIOService;
+import com.employeepayroll.modal.EmployeePayrollData;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayrollService {
+    public enum IOService {
+        CONSOLE_IO, FILE_IO, DB_IO, REST_IO
+    }
+
+    public static final Scanner SC = new Scanner(System.in);
+    private List<EmployeePayrollData> employeeList;
+
+    public EmployeePayrollService() {
+        this.employeeList = new ArrayList<EmployeePayrollData>();
+    }
+
+    public EmployeePayrollService(List<EmployeePayrollData> employeeList) {
+        this.employeeList = employeeList;
+    }
+
+    public int sizeOfEmployeeList() {
+        return this.employeeList.size();
+    }
+
+    public void readEmployeeData(IOService ioType) {
+        if (ioType.equals(IOService.CONSOLE_IO)) {
+            System.out.println("Enter employee id:");
+            int employeeId = SC.nextInt();
+            System.out.println("Enter employee name:");
+            SC.nextLine();
+            String employeeName = SC.nextLine();
+            System.out.println("Enter employee salary:");
+            double employeeSalary = SC.nextDouble();
+            EmployeePayrollData newEmployee = new EmployeePayrollData(employeeId, employeeName, employeeSalary);
+            employeeList.add(newEmployee);
+        } else if (ioType.equals(IOService.FILE_IO)) {
+            this.employeeList = new FileIOService().readData();
+        }
+    }
+
+    public void writeEmployeeData(IOService ioType) {
+        if (ioType.equals(IOService.CONSOLE_IO)) {
+            for (EmployeePayrollData o : employeeList)
+                System.out.println(o.toString());
+        } else if (ioType.equals(IOService.FILE_IO)) {
+            new FileIOService().writeData(employeeList);
+        }
+    }
+
+    public long countEnteries(IOService ioType) {
+        if (ioType.equals(IOService.FILE_IO))
+            return new FileIOService().countEntries();
+        return 0;
+    }
+
+    public void printEmployeePayrollData() {
+        new FileIOService().printEmployeePayrolls();
+    }
+}
---
+++ src/test/java/com/employeepayroll/EmployeePayrollServiceTest.java
@@ -0,0 +1,33 @@
+package com.employeepayroll;
+
+import com.employeepayroll.modal.EmployeePayrollData;
+import com.employeepayroll.payrollservice.EmployeePayrollService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class EmployeePayrollServiceTest {
+    @Test
+    public void given3EmployeesWhenWrittenToFileShouldMatchNumberOfEmployeeEntries() {
+        EmployeePayrollData[] arrayOfEmployees = {
+                new EmployeePayrollData(1, "Aditya Verma", 800000.0),
+                new EmployeePayrollData(2, "Akhil Singh", 850000.0),
+                new EmployeePayrollData(3, "Anamika Bhatt", 900000.0) };
+
+        EmployeePayrollService payrollServiceObject = new EmployeePayrollService(Arrays.asList(arrayOfEmployees));
+        payrollServiceObject.writeEmployeeData(EmployeePayrollService.IOService.FILE_IO);
+        payrollServiceObject.printEmployeePayrollData();
+
+        Assertions.assertEquals(3, payrollServiceObject.countEnteries(EmployeePayrollService.IOService.FILE_IO));
+    }
+
+    @Test
+    public void given3EmployeesWhenReadFromFileShouldMatchNumberOfEmployeeEntries() {
+
+        EmployeePayrollService payrollServiceObject = new EmployeePayrollService();
+        payrollServiceObject.readEmployeeData(EmployeePayrollService.IOService.FILE_IO);
+        int countOfEntriesRead = payrollServiceObject.sizeOfEmployeeList();
+        Assertions.assertEquals(3, countOfEntriesRead);
+    }
+}
